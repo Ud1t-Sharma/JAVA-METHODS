3d6bdff2553bdc48c687a7163a454c064e780986
Udit Sharma <121646116+Ud1t-Sharma@users.noreply.github.com>
2023-07-31 21:29:38 +0530

    Add files via upload

---
+++ m2.java
@@ -0,0 +1,10 @@
+class m2{
+    static void  speed (int a){// "a" is a argument.
+        System.out.println( a + 817);
+    }
+    public static void main (String[] args){
+        var b = "Information can be passed to methods as parameter. Parameters act as variables inside the method.\n\nParameters are specified after the method name, inside the parentheses. You can add as many parameters as you want, just separate them with a comma.";
+        System.out.println(b);
+        speed(665);//adds 665 to 817
+
+}}
