7047053c6f28f44661e995238d3be4a414d13750
Udit Sharma <121646116+Ud1t-Sharma@users.noreply.github.com>
2023-08-01 23:44:58 +0530

    Add files via upload

--- m2.java
+++ m2.java
@@ -1,15 +1,19 @@
 class m2{
-    static void  speed (int a , )// "a" is an argument.
+    static void  speed (int a)// "a" is an argument.
     {
         System.out.println(817 + a);
-        System.out.println(a);
-        System.out.println(d);
+        if (a>888){
+            System.out.println("noIce");
+        }
+        else {
+            System.out.println("more noIce");
+        }
+        
 
     }
     public static void main (String[] args){
         var b = "Information can be passed to methods as parameter. Parameters act as variables inside the method.\n\nParameters are specified after the method name, inside the parentheses. You can add as many parameters as you want, just separate them with a comma.";
         System.out.println(b);
         speed(665);//adds 665 to 817
-        speed()
 
 }}
